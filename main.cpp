72797a0c268c9139b0dc8acf8715b1b39e381511
893374759 <893374759@qq.com>
2022-12-28 01:29:29 -0500

    Format.

--- main.cpp
+++ main.cpp
@@ -26,22 +26,23 @@
 #else
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>  // Functions for manipulating numeric IP addresses.
-#include <netdb.h>      // Name resolution
+#include <arpa/inet.h> // Functions for manipulating numeric IP addresses.
+#include <netdb.h>     // Name resolution
 #endif
 
 #include "progressbar.hpp"
 #include "hash-uppers-downers/sha1.h"
 
 /**** /hash-uppers-downers/sha1.c *****************************************************************************************/
-void
-digest_to_hex(const uint8_t digest[SHA1_DIGEST_SIZE], char *output)
+void digest_to_hex(const uint8_t digest[SHA1_DIGEST_SIZE], char *output)
 {
     int i, j;
     char *c = output;
 
-    for (i = 0; i < SHA1_DIGEST_SIZE / 4; i++) {
-        for (j = 0; j < 4; j++) {
+    for (i = 0; i < SHA1_DIGEST_SIZE / 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
             sprintf(c, "%02X", digest[i * 4 + j]);
             c += 2;
         }
@@ -56,72 +57,85 @@ digest_to_hex(const uint8_t digest[SHA1_DIGEST_SIZE], char *output)
  * Connect "nc uppers-downers.hack.fe-ctf.dk 1337"
  * @return int socket ID
  */
-int ConnectSocket(){  // nc uppers-downers.hack.fe-ctf.dk 1337
+int ConnectSocket()
+{ // nc uppers-downers.hack.fe-ctf.dk 1337
     // socket
     int client = socket(AF_INET, SOCK_STREAM, 0);
-    if (client == -1) {
+    if (client == -1)
+    {
         std::cout << "Error: socket" << std::endl;
         return 0;
     }
     struct sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(1337);
-    serverAddr.sin_addr.s_addr = inet_addr("34.154.114.69"); 
-    if (connect(client, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+    serverAddr.sin_addr.s_addr = inet_addr("34.154.114.69");
+    if (connect(client, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+    {
         std::cout << "Error: connect" << std::endl;
         return -1;
     }
     return client;
 }
 
-const char passw_lib[] ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; // password char list
-struct timeval t_start;  // global time cost
-unsigned int ServerReqCount = 0, SHA1count = 0;  // server call count, hash preparation count 
+const char passw_lib[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; // password char list
+struct timeval t_start;                                                                    // global time cost
+unsigned int ServerReqCount = 0, SHA1count = 0;                                            // server call count, hash preparation count
 
 /**
- * Implementation of a 
+ * Implementation of a
  * dict{
  *   password: hash_digest
  * }
  * strlen(passw) := 5
  */
-typedef struct {
+typedef struct
+{
     char passw[6] = {0};
     char userh[SHA1_DIGEST_SIZE] = {0};
-} HASHATTEMPT;  // 26 Bytes
+} HASHATTEMPT; // 26 Bytes
 
 /**
  * 3 digits, mod 62 counter for look-up
  *   MSB[base4, base3, base2]LSB
  */
-class mod62counter3{
+class mod62counter3
+{
 public:
-    uint8_t base2 = 0;  // 0 ~ 61
-    uint8_t base3 = 0;  // 0 ~ 61
-    uint8_t base4 = 0;  // 0 ~ 61
+    uint8_t base2 = 0; // 0 ~ 61
+    uint8_t base3 = 0; // 0 ~ 61
+    uint8_t base4 = 0; // 0 ~ 61
 
-    mod62counter3(void){
-        base2 = base3 = base4 = 0;  // init
+    mod62counter3(void)
+    {
+        base2 = base3 = base4 = 0; // init
     };
 
     // increase, carry
-    void increase(void){
-        if(61 == base2){
+    void increase(void)
+    {
+        if (61 == base2)
+        {
             base2 = 0;
-            if(61 == base3){
+            if (61 == base3)
+            {
                 base3 = 0;
-                if(61 == base4){
+                if (61 == base4)
+                {
                     base4 = 0;
                 }
-                else{
+                else
+                {
                     ++base4;
                 }
             }
-            else{
+            else
+            {
                 ++base3;
             }
         }
-        else{
+        else
+        {
             ++base2;
         }
     };
@@ -133,9 +147,10 @@ public:
  * @param[in] b: pointer to a HASHATTEMPT object b
  * @return memcmp userhash a and b
  */
-int HASHATTEMPTcmpfunc (const void * a, const void * b){
-    HASHATTEMPT* pa = (HASHATTEMPT*)a;
-    HASHATTEMPT* pb = (HASHATTEMPT*)b;
+int HASHATTEMPTcmpfunc(const void *a, const void *b)
+{
+    HASHATTEMPT *pa = (HASHATTEMPT *)a;
+    HASHATTEMPT *pb = (HASHATTEMPT *)b;
     return memcmp(pa->userh, pb->userh, sizeof(pa->userh));
 }
 
@@ -146,7 +161,8 @@ int HASHATTEMPTcmpfunc (const void * a, const void * b){
  * @param[in] password: password string payload that sent to server to compare
  * @return '>', '<', or exit(0)
  */
-char ServerCmp(const int client, const char* password){
+char ServerCmp(const int client, const char *password)
+{
     assert(strlen(password) == 5);
     char payload_password[6] = {0, 0, 0, 0, 0, '\n'};
     memcpy(payload_password, password, 5);
@@ -155,9 +171,10 @@ char ServerCmp(const int client, const char* password){
     char SocketBuf[255];
     recvlen = recv(client, SocketBuf, sizeof(SocketBuf), 0);
     SocketBuf[recvlen] = 0;
-    char cmp = SocketBuf[0];  // server return '>' or '<'
+    char cmp = SocketBuf[0]; // server return '>' or '<'
 
-    if(cmp != '>' && cmp != '<'){  // server return flag
+    if (cmp != '>' && cmp != '<')
+    { // server return flag
         std::cout << SocketBuf << std::endl;
         std::cout << "* Real Password       = " << password << std::endl;
         struct timeval t_end;
@@ -170,7 +187,7 @@ char ServerCmp(const int client, const char* password){
         return 0;
     }
 
-    recvlen = recv(client, SocketBuf, sizeof(SocketBuf), 0);  // recv "Password:"
+    recvlen = recv(client, SocketBuf, sizeof(SocketBuf), 0); // recv "Password:"
     SocketBuf[recvlen] = 0;
 
     ++ServerReqCount;
@@ -192,32 +209,36 @@ char ServerCmp(const int client, const char* password){
  * @param[in] salt: powerter to salt string
  * @return number of potential password payloads
  */
-unsigned int GenerateHashSample(HASHATTEMPT* HashSampleArray, const unsigned int HashSampleArrayLen, 
-                        unsigned char* low, unsigned char* high, mod62counter3 &base, SHA1_CTX* ctx, const char* salt){
+unsigned int GenerateHashSample(HASHATTEMPT *HashSampleArray, const unsigned int HashSampleArrayLen,
+                                unsigned char *low, unsigned char *high, mod62counter3 &base, SHA1_CTX *ctx, const char *salt)
+{
 
     unsigned int HashSampleArrayCounter = 0;
     char password_tmp[6] = {0};
     char userhash_tmp[SHA1_DIGEST_SIZE] = {0};
     // generate hashes in passw_lib order
     assert(HashSampleArrayLen == 62 * 62);
-    for(unsigned int j = 0; j < 62; ++j){
-        for(unsigned int k = 0; k < 62; ++k){
+    for (unsigned int j = 0; j < 62; ++j)
+    {
+        for (unsigned int k = 0; k < 62; ++k)
+        {
             password_tmp[0] = passw_lib[k];
             password_tmp[1] = passw_lib[j];
             password_tmp[2] = passw_lib[base.base2];
             password_tmp[3] = passw_lib[base.base3];
             password_tmp[4] = passw_lib[base.base4];
             SHA1_Init(ctx);
-            SHA1_Update(ctx, (uint8_t*)salt, strlen(salt));
-            SHA1_Update(ctx, (uint8_t*)password_tmp, strlen(password_tmp));
-            SHA1_Final(ctx, (uint8_t*)userhash_tmp);
+            SHA1_Update(ctx, (uint8_t *)salt, strlen(salt));
+            SHA1_Update(ctx, (uint8_t *)password_tmp, strlen(password_tmp));
+            SHA1_Final(ctx, (uint8_t *)userhash_tmp);
 
             // only save the password which is in the range, omit out-of-range passwords
-            if(memcmp(userhash_tmp, low, sizeof(userhash_tmp)) > 0 && memcmp(userhash_tmp, high, sizeof(userhash_tmp)) < 0){
+            if (memcmp(userhash_tmp, low, sizeof(userhash_tmp)) > 0 && memcmp(userhash_tmp, high, sizeof(userhash_tmp)) < 0)
+            {
                 memcpy(HashSampleArray[HashSampleArrayCounter].passw, password_tmp, sizeof(password_tmp));
                 memcpy(HashSampleArray[HashSampleArrayCounter].userh, userhash_tmp, sizeof(userhash_tmp));
                 ++HashSampleArrayCounter;
-            }            
+            }
         }
     }
     base.increase();
@@ -238,34 +259,42 @@ unsigned int GenerateHashSample(HASHATTEMPT* HashSampleArray, const unsigned int
  * @param[in, out] low: lower bound of the hash digest
  * @param[in, out] high: upper bound of the hash digest
  */
-void CheckNewRange(const HASHATTEMPT* SortedHashSampleArray, const unsigned int ArrayLen, const int client, 
-                    unsigned char* low, unsigned char* high){
+void CheckNewRange(const HASHATTEMPT *SortedHashSampleArray, const unsigned int ArrayLen, const int client,
+                   unsigned char *low, unsigned char *high)
+{
 
-    unsigned int low_idx = 0, high_idx = ArrayLen-1;
+    unsigned int low_idx = 0, high_idx = ArrayLen - 1;
     unsigned int mid_idx = (low_idx + high_idx) / 2;
     char cmp = 0;
     // compare and find a new hash range [low, high]
-    if(ArrayLen == 0){
+    if (ArrayLen == 0)
+    {
         return;
     }
-    if(ServerCmp(client, SortedHashSampleArray[0].passw) == '>'){  // good < user[0]
+    if (ServerCmp(client, SortedHashSampleArray[0].passw) == '>')
+    { // good < user[0]
         memcpy(high, SortedHashSampleArray[0].userh, sizeof(high));
         return;
     }
-    else if(ServerCmp(client, SortedHashSampleArray[ArrayLen-1].passw) == '<'){  // user[largest] < good
-        memcpy(low, SortedHashSampleArray[ArrayLen-1].userh, sizeof(low));
+    else if (ServerCmp(client, SortedHashSampleArray[ArrayLen - 1].passw) == '<')
+    { // user[largest] < good
+        memcpy(low, SortedHashSampleArray[ArrayLen - 1].userh, sizeof(low));
         return;
     }
-    else{
-        while(mid_idx > low_idx){
+    else
+    {
+        while (mid_idx > low_idx)
+        {
             cmp = ServerCmp(client, SortedHashSampleArray[mid_idx].passw);
-            if(cmp == '>'){ // good < user[mid]
+            if (cmp == '>')
+            { // good < user[mid]
                 high_idx = mid_idx;
             }
-            else if(cmp == '<'){  // user[mid] < good
+            else if (cmp == '<')
+            { // user[mid] < good
                 low_idx = mid_idx;
             }
-            mid_idx = (high_idx + low_idx) / 2;  // a new middle point
+            mid_idx = (high_idx + low_idx) / 2; // a new middle point
         }
         // update
         memcpy(low, SortedHashSampleArray[mid_idx].userh, sizeof(low));
@@ -273,59 +302,66 @@ void CheckNewRange(const HASHATTEMPT* SortedHashSampleArray, const unsigned int 
     }
 }
 
-int main() {
-	SHA1_CTX ctx;
-	unsigned char goodhash[SHA1_DIGEST_SIZE] = {0};
-	unsigned char userhash[SHA1_DIGEST_SIZE] = {0};
-	char password[256] = {0};
+int main()
+{
+    SHA1_CTX ctx;
+    unsigned char goodhash[SHA1_DIGEST_SIZE] = {0};
+    unsigned char userhash[SHA1_DIGEST_SIZE] = {0};
+    char password[256] = {0};
     uint8_t digest[20] = {0};
 
-	char salt[16] = "pkDHTxmMR18N2l9";  // My static salt
+    char salt[16] = "pkDHTxmMR18N2l9"; // My static salt
     salt[15] = 0;
-    
+
     // socket
     int client = ConnectSocket();
     char SocketBuf[255] = {0};
-    int recvlen = 0; 
+    int recvlen = 0;
 
     // get salt
-    for(unsigned j = 0; j < 3; ++j){
+    for (unsigned j = 0; j < 3; ++j)
+    {
         recvlen = recv(client, SocketBuf, sizeof(SocketBuf), 0);
         SocketBuf[recvlen] = 0;
         std::cout << SocketBuf;
-        if(j == 1){
-            if(memcmp(SocketBuf + 6, salt, sizeof(salt) - 1) != 0){  // get your salt
+        if (j == 1)
+        {
+            if (memcmp(SocketBuf + 6, salt, sizeof(salt) - 1) != 0)
+            { // get your salt
                 memcpy(salt, SocketBuf + 6, strlen(salt));
             }
         }
-        if(j == 2){
-            std::cout << std::endl << "* Your salt           = " << salt << std::endl;
+        if (j == 2)
+        {
+            std::cout << std::endl
+                      << "* Your salt           = " << salt << std::endl;
         }
         fflush(stdout);
     }
 
-    HASHATTEMPT HashSamples[62*62] = {0};
+    HASHATTEMPT HashSamples[62 * 62] = {0};
     mod62counter3 HashSamplesBase;
 
     // init range for hash (160 bits): hashlo ~ hashhi
-	unsigned char hashlo[SHA1_DIGEST_SIZE] = {0};
-	unsigned char hashhi[SHA1_DIGEST_SIZE] = {0};
-    for(unsigned j = 0; j < SHA1_DIGEST_SIZE; ++j){
+    unsigned char hashlo[SHA1_DIGEST_SIZE] = {0};
+    unsigned char hashhi[SHA1_DIGEST_SIZE] = {0};
+    for (unsigned j = 0; j < SHA1_DIGEST_SIZE; ++j)
+    {
         hashhi[j] = 0 - 1;
     }
 
     unsigned int HashSampleLen = 0;
     progressbar bar(62 * 62 * 62);
     gettimeofday(&t_start, NULL);
-    for(unsigned j = 0; j < 62 * 62 * 62; ++j){
+    for (unsigned j = 0; j < 62 * 62 * 62; ++j)
+    {
         bar.update();
         // prepare potential passwords in range [hashlo, hashhi]
-        HashSampleLen = GenerateHashSample(HashSamples, sizeof(HashSamples)/sizeof(HASHATTEMPT), hashlo, hashhi, HashSamplesBase, &ctx, salt);
+        HashSampleLen = GenerateHashSample(HashSamples, sizeof(HashSamples) / sizeof(HASHATTEMPT), hashlo, hashhi, HashSamplesBase, &ctx, salt);
         // access server to compare the passwords, get new range
         CheckNewRange(HashSamples, HashSampleLen, client, hashlo, hashhi);
     }
 
     close(client);
-	return 0;
+    return 0;
 }
-
